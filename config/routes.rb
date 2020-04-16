bc31c3c24faf4cef146cac9ff27f035d135ebd55
Daisuke Shibuya1006 <shibudai1006@yahoo.co.jp>
2020-04-16 23:44:16 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  # root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'persons/new'
+  root 'static_pages#home'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
 
   resources :microposts
   resources :users
   get '/posts' => "post#index"
+  get 'posts/index'
+  get '/signup' => 'persons#new'
 end
