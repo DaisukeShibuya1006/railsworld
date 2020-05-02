07090b7fe2356af3da6d29f3f8a93916fef9dd65
Daisuke Shibuya1006 <shibudai1006@yahoo.co.jp>
2020-05-02 20:07:56 +0900

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_134103) do
+ActiveRecord::Schema.define(version: 2020_05_02_102700) do
 
   create_table "blogers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_04_29_134103) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
